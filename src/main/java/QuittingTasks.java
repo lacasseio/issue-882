6d962b086e75b108790bbb200d2f03a2838d7e2c
Daniel Lacasse <daniel@lacasse.io>
2016-12-30 11:16:32 -0800

    Initial sample

---
+++ src/main/java/QuittingTasks.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.util.stream.*;
+import java.util.concurrent.*;
+
+public class QuittingTasks {
+  public static final int COUNT = 690;
+  public static void main(String[] args) throws InterruptedException {
+    ExecutorService es =
+      Executors.newCachedThreadPool();
+	  
+    for (int i = 0; i < COUNT; ++i) {
+        es.execute(new Runnable() {
+            @Override
+            public void run() {
+                System.out.print("d ");
+            }
+        });
+    }
+    es.shutdown();
+  }
+}
---
+++ src/main/java/SingleThreadExecutor2.java
@@ -0,0 +1,7 @@
+// concurrent/SingleThreadExecutor2.java
+// (c)2016 MindView LLC: see Copyright.txt
+// We make no guarantees that this code is fit for any purpose.
+// Visit http://OnJava8.com for more book information.
+public class SingleThreadExecutor2 {
+  public static void main(String[] args) {}
+}
